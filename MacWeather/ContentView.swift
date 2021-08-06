ce3020032d55c8c992f3452053e886ef831469d6
sharansajivmenon <sharansajivmenon@gmail.com>
2021-08-06 09:13:13 -0400

    Initial Commit

---
+++ MacWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MacWeather
+//
+//  Created by Sharan Sajiv Menon on 8/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MacWeather/MacWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacWeatherApp.swift
+//  MacWeather
+//
+//  Created by Sharan Sajiv Menon on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct MacWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
